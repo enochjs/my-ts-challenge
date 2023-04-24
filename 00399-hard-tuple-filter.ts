5a864b7336f477327c1a78cf2a732a236f4d5242
枫荷 <fenghe@linkmore.com>
2023-04-24 11:42:14 +0800

    fix(main): optimiz tuple fileter

--- 00399-hard-tuple-filter.ts
+++ 00399-hard-tuple-filter.ts
@@ -6,19 +6,25 @@ type cases = [
   Expect<Equal<FilterOut<[never], never>, []>>,
   Expect<Equal<FilterOut<['a', never], never>, ['a']>>,
   Expect<Equal<FilterOut<[1, never, 'a'], never>, [1, 'a']>>,
-  Expect<Equal<FilterOut<[never, 1, 'a', undefined, false, null], never | null | undefined>, [1, 'a', false]>>,
+  Expect<Equal<FilterOut<[never, 1, 'a', undefined, false, null], number | never | null | undefined>, [1, 'a', false]>>,
   Expect<Equal<FilterOut<[number | null | undefined, never], never | null | undefined>, [number | null | undefined]>>,
 ]
 
 
 // ============= Your Code Here =============
-
-
+type MyInclude<T, V, U = T> = 
+  T extends U
+    ? Equal<T, V> extends true
+      ? true
+      : never
+    : never
 
 type FilterOut<T extends any[], V, R extends any[] = []> =
   T extends [infer F, ...infer Rest]
-    ? [F] extends [V]
+    ? [F] extends [never]
       ? FilterOut<Rest, V, R>
-      : FilterOut<Rest, V, [...R, F]>
+      : Equal<MyInclude<V, F>, true> extends true
+        ? FilterOut<Rest, V, R>
+        : FilterOut<Rest, V, [...R, F]>
     : R
 
